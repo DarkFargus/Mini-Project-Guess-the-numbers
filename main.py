4c270472793fac3ec27aea5ed001fb3b3cfb0a77
DarkFargus <sheronoc@mail.ru>
2022-01-07 03:15:57 +0300

    project creation

---
+++ main.py
@@ -0,0 +1,26 @@
+from random import *
+#from math import trunc
+
+random_number = randint(1, 100)
+user_number = 0
+print('Добро пожаловать в числовую угадайку!')
+
+#Функция для проверки вводимого числа.
+def is_valid(num):
+    if 1 <= user_number <= 100:
+        return True
+    else:
+        return False
+
+while user_number != random_number:
+    user_number = int(input())
+    if is_valid(user_number) == False:
+        print('А может быть все-таки введем целое число от 1 до 100?')
+        continue
+    elif user_number < random_number:
+        print('Ваше число меньше загаданного, попробуйте еще разок')
+    elif user_number > random_number:
+        print('Ваше число больше загаданного, попробуйте еще разок')
+    elif user_number == random_number:
+        print('Вы угадали, поздравляем!')
+        print('Спасибо, что играли в числовую угадайку! Еще увидимся...')
