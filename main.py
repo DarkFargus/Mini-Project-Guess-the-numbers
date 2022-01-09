cc969165491dfa1f21c2c296c307c0077bd9c337
DarkFargus <sheronoc@mail.ru>
2022-01-09 18:00:23 +0300

    Новый функционал: подсчет попыток, запуск новой игры - после угадывания числа, указание правой границы выбора числа.

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from random import *
-#from math import trunс
 
-random_number = randint(1, 100)
 user_number = 0
-print('Добро пожаловать в числовую угадайку!')
+attempts = 0
 
-#Функция для проверки вводимого числа.??
+print('Добро пожаловать в числовую угадайку!')
+print('Суть игры - угадать целое число от 1 до "n", которое загадала программа.')
+print('Введите правую границу, для случайного выбора числа - "n"')
+
+right_border = int(input())
+random_number = randint(1, right_border)
+
+#Функция для проверки вводимого числа.
 def is_valid(num):
-    if 1 <= user_number <= 100:
+    if 1 <= user_number <= right_border:
         return True
     else:
         return False
@@ -19,8 +24,16 @@ while user_number != random_number:
         continue
     elif user_number < random_number:
         print('Ваше число меньше загаданного, попробуйте еще разок')
+        attempts += 1
     elif user_number > random_number:
         print('Ваше число больше загаданного, попробуйте еще разок')
+        attempts += 1
     elif user_number == random_number:
         print('Вы угадали, поздравляем!')
+        print('Количество попыток -', attempts)
         print('Спасибо, что играли в числовую угадайку! Еще увидимся...')
+        print('Введите правую границу, для случайного выбора числа - "n", чтобы начать новую игру.')
+        right_border = int(input())
+        random_number = randint(1, right_border)
+        attempts = 0
+        continue
